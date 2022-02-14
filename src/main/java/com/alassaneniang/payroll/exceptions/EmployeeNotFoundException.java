099ce4bcf698738e50f59b849e21b33f131a6c4f
Alassane NIANG <adniang75@gmail.com>
2022-02-14 11:24:22 +0100

    Building the foundation of our REST service (not yet REST, just RPC!!!!)

---
+++ src/main/java/com/alassaneniang/payroll/exceptions/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.alassaneniang.payroll.exceptions;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException ( Long id ) {
+        super( "Could not find employee " + id );
+    }
+
+}
---
+++ src/main/java/com/alassaneniang/payroll/models/Employee.java
@@ -0,0 +1,28 @@
+package com.alassaneniang.payroll.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String role;
+
+    public Employee ( String name, String role ) {
+        this.name = name;
+        this.role = role;
+    }
+
+}
